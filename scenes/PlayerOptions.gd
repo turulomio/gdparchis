0a9a67485e6201fa7803f47722ec5896616082cf
turulomio <turulomio@yahoo.es>
2024-05-13 21:05:23 +0100

    Improved Icons, versions, player options and menus (#67)
    * Added icons and layouts to main
    
    * Options is now centered
    
    * works player selection
    
    * Improving ui of Playeroptions

--- scenes/PlayerOptions.gd
+++ scenes/PlayerOptions.gd
@@ -1,39 +1,50 @@
-
 extends Control
+class_name PlayerOptions
 
 
 @export var playername: String="player"  : set = set_playername
 @export var plays: bool= true : set = set_plays
 @export var ia:bool=true: set=set_ia
 # Editor will enumerate with string names.
-@export_enum("YELLOW", "BLUE", "RED", "GREEN") var color_name  : set =  set_color_name ## Estaba con string
-
-func set_color_name(s):	
-	color_name=s
-	if Engine.is_editor_hint(): #Solo se ejecuta como tool
+@export var id: int=0:
+	set(value):
+		id=value
 		change_icon_and_name()
 
-		set_plays(plays)
-		set_ia(ia)
+#func set_color_name(s):	
+	#color_name=s
+	#if Engine.is_editor_hint(): #Solo se ejecuta como tool
+		#change_icon_and_name()
+
+		#set_plays(plays)
+		#set_ia(ia)
 
 
 func change_icon_and_name():
-	if color_name=="Red":
+	if id==Globals.ePlayer.RED:
 		set_playername("Redy")
 		if has_node("HBoxContainer/Icon"):
 			get_node("HBoxContainer/Icon").texture = load("res://images/ficharoja.png")
-	if color_name=="Blue":
+	if id==Globals.ePlayer.BLUE:
 		set_playername("Bluey")
 		if has_node("HBoxContainer/Icon"):
 			get_node("HBoxContainer/Icon").texture = load("res://images/fichaazul.png")
-	if color_name=="Green":
+	if id==Globals.ePlayer.GREEN:
 		set_playername("Greeny")
 		if has_node("HBoxContainer/Icon"):
 			get_node("HBoxContainer/Icon").texture = load("res://images/fichaverde.png")
-	if color_name=="Yellow":
+	if id==Globals.ePlayer.YELLOW:
 		set_playername("Yellowy")
 		if has_node("HBoxContainer/Icon"):
 			get_node("HBoxContainer/Icon").texture = load("res://images/fichaamarilla.png")
+	$HBoxContainer/Plays.set("theme_override_colors/font_color", Globals.ePlayer2Color(id))
+	$HBoxContainer/Plays.set("theme_override_colors/font_pressed_color", Globals.ePlayer2Color(id))
+	$HBoxContainer/Plays.set("theme_override_colors/font_hover_color", Globals.ePlayer2Color(id))
+	$HBoxContainer/Plays.set("theme_override_colors/font_hover_pressed_color", Globals.ePlayer2Color(id))
+	$HBoxContainer/IA.set("theme_override_colors/font_color", Globals.ePlayer2Color(id))
+	$HBoxContainer/IA.set("theme_override_colors/font_pressed_color", Globals.ePlayer2Color(id))
+	$HBoxContainer/IA.set("theme_override_colors/font_hover_color", Globals.ePlayer2Color(id))
+	$HBoxContainer/IA.set("theme_override_colors/font_hover_pressed_color", Globals.ePlayer2Color(id))
 	
 func set_playername(s):
 	playername=s
@@ -52,9 +63,9 @@ func set_ia(b):
 	
 func _ready():
 	change_icon_and_name()
-	set_plays(plays)
-	set_ia(ia)
-	set_playername(playername)
+	#set_plays(plays)
+	#set_ia(ia)
+	#set_playername(playername)
 
 
 func _on_Plays_toggled(button_pressed):
