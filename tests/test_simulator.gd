54bb2fb8d9672f75aad54eb6e8bf041e9fe16c9b
turulomio <turulomio@yahoo.es>
2026-08-16 09:50:29 +0200

    Better colors

--- tests/test_simulator.gd
+++ tests/test_simulator.gd
@@ -62,7 +62,7 @@ func get_piece(player_id: int, piece_id: int):
 	return null
 
 
-## Frees memory by queueing node deletion for simulator board node.
+## Frees memory by explicitly freeing simulator board node and all child players/pieces.
 func cleanup():
 	if self.board_node and is_instance_valid(self.board_node):
-		self.board_node.queue_free()
+		self.board_node.free()
